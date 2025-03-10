92f0b75ef1d90fd9cf522b28919c7534c120e58f
Kashish268 <khkoshiya@gmail.com>
2025-03-11 00:34:36 +0530

    update footer , complete home page , wishlist , properties

---
+++ Models/PropertyViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication1.Models
+{
+    public class PropertyViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Location { get; set; }
+        public string Price { get; set; }
+        public double Rating { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
