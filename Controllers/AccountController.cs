d8cce9b2ce75ff5efeb85dbd5d668ab2f99ff90a
Kashish268 <khkoshiya@gmail.com>
2025-03-18 19:18:31 +0530

    add room owner panel and super admin panel with static login

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginModel model)
+        {
+            if (model.Email == "admin@gmail.com" && model.Password == "111")
+            {
+                return RedirectToAction("Dashboard", "Account");
+                
+            }
+
+            else if (model.Email == "superadmin@gmail.com" && model.Password == "123") {
+                return RedirectToAction("Super_AdminDashboard", "Account");
+            }
+
+            return View();
+        }
+
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public IActionResult Super_AdminDashboard()
+        {
+            return View();
+        }
+
+    }
+}
