00c002a936cb28fae7fe93454fbeee62127ba45d
HEMANSHI GARNARA <hemanshigarnara2004@gmail.com>
2025-03-22 11:52:36 +0530

    add code of super admin dashboard

--- Controllers/Super_AdminController.cs
+++ Controllers/Super_AdminController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using System.Collections.Generic;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
@@ -6,6 +10,7 @@ namespace WebApplication1.Controllers
     {
         public IActionResult Super_AdminDashboard()
         {
+            
             return View();
         }
     }
