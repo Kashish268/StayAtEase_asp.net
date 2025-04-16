04372e5cf58841b6ce905dc8f696c35abb682c0f
HEMANSHI GARNARA <hemanshigarnara2004@gmail.com>
2025-04-16 20:33:11 +0530

    update code of super admin dashboard

--- Models/SuperAdminDashboardViewModel.cs
+++ Models/SuperAdminDashboardViewModel.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using WebApplication1.Models;
+
+public class SuperAdminDashboardViewModel
+{
+    public int TotalProperties { get; set; }
+    public int ActiveListings { get; set; }
+    public int TotalInquiries { get; set; }
+    public double AverageReviewRating { get; set; }
+
+    public List<User> Tenants { get; set; } = new();
+    public List<User> PropertyOwners { get; set; } = new();
+    public List<PropertyMessageViewModel> LatestMessages { get; set; } = new();
+}
+
